b0c98d699731c2589710a246aa714e841e6b7593
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-07-22 17:23:57 -0500

    Finished Assignment

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,15 @@
+require 'HTTParty'
+
 class Kele
+include HTTParty
+
+
   def initialize(username, password) 
-  	username = @username
-  	password = @password
+  	@base_uri = "https://www.bloc.io/api/v1"
+  	response = Kele.post("#{@base_uri}/sessions",body: {email: username, password: password})
+  	@auth_token = response["auth_token"]
+  	if @auth_token == nil
+  	  raise 'Invalide Credentials'
+  	end
   end
 end
