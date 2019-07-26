76f9864ed99a53845bb19b012b3924f7d7ce9e06
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-07-26 16:59:49 -0500

    Finished Mentor Availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,6 +4,7 @@ require 'json'
 class Kele
 include HTTParty
 
+# mentor_id"=>2292457
 
   def initialize(username, password) 
   	@base_uri = "https://www.bloc.io/api/v1"
@@ -19,4 +20,14 @@ include HTTParty
   	obj = JSON.parse(response.body)
   	p obj.class   
   end  
+  
+  def get_mentor_availability(mentor_id)
+  	# https://private-anon-56a5d4037e-blocapi.apiary-mock.com/api/v1/mentors/id/student_availability
+    response = self.class.get("#{@base_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    availability = JSON.parse(response.body)["slots"]
+    p availability.class
+    p availability 
+  end
+
+
 end
