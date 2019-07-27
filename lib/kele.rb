40c812c7b41336dd7e6d5303163a69ac6fa48424
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-07-27 12:18:10 -0500

    updated checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -17,15 +17,13 @@ include HTTParty
 
   def get_me
   	response = self.class.get("#{@base_uri}/users/me", headers: { "authorization" => @auth_token })
-  	obj = JSON.parse(response.body)
-  	p obj.class   
+  	obj = JSON.parse(response.body)  
   end  
   
   def get_mentor_availability(mentor_id)
   	# https://private-anon-56a5d4037e-blocapi.apiary-mock.com/api/v1/mentors/id/student_availability
     response = self.class.get("#{@base_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     availability = JSON.parse(response.body)["slots"]
-    p availability.class
   end
 
 
